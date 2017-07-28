5800101b810ebe3a139fcfe691969203709e4c08
Miguel Brito <mbrito1086@gmail.com>
2017-07-28 13:56:50 -0400

    HTML and JS work

---
+++ main.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const mustacheExpress = require('mustache-express')
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+
+const app = express()
+
+app.engine('mst', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mst')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(expressValidator())
+
+app.get('/', (req, resp) => {
+    resp.render('home')
+})
+
+const todos = [
+  "Wash the car"
+];
+
+app.get("/", function (req, res) {
+  res.render('index', { todos: todos });
+});
+
+app.post("/", function (req, res) {
+  todos.push(req.body.todo);
+  res.redirect('/');
+})
+
+
+
+
+app.listen(3000, () => {
+  console.log("Magic is happening on port 3000")
+});
