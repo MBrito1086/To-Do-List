827341289a67b151eee491685f1a0310451ce160
Miguel Brito <mbrito1086@gmail.com>
2017-07-29 20:31:01 -0400

    Trying to figure out how to mark Completes

--- main.js
+++ main.js
@@ -15,22 +15,56 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(expressValidator())
 
+const todosList = []
+const compList = []
+
 app.get('/', (req, resp) => {
-    resp.render('home')
+  resp.render('home', { todosList: todosList } );
 })
 
-const todos = [
-  "Wash the car"
-];
-
-app.get("/", function (req, res) {
-  res.render('index', { todos: todos });
-});
-
-app.post("/", function (req, res) {
-  todos.push(req.body.todo);
-  res.redirect('/');
+app.get('/', (req, resp) => {
+  resp.render('home', { compList: compList } )
 })
+
+app.post('/', (req, resp) => {
+
+  req
+    .checkBody("todo", "You must add a To Do")
+    .notEmpty();
+
+  const errors = req.validationErrors()
+
+  if (errors) {
+    resp.send("Nothing To Do")
+  } else {
+    todosList.push(req.body.todo)
+  }
+  resp.redirect('/')
+})
+
+app.post('/markCom', (req, resp) => {
+    req
+    .checkBody("done")
+  compList.push(req.params.done)
+  console.log("params: " + req.body.done)
+  console.log("going to compList: " + req.params.done)
+  resp.redirect('/')
+})
+
+
+
+// const todos = [
+//   "Wash the car"
+// ];
+//
+// app.get("/", function (req, res) {
+//   res.render('index', { todos: todos });
+// });
+//
+// app.post("/", function (req, res) {
+//   todos.push(req.body.todo);
+//   res.redirect('/');
+// })
 
 
 
